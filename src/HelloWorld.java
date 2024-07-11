28e9e87e0631b570665a79600548a941a5b7fa85
chera <hiiamdiv@yandex.ru>
2024-07-11 18:33:33 +0300

    UPDATED SEXXXXXXXXX

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello bitches");
+		printer();
+	}
+	public static printer() {
+		System.out.println("Hello guys");
 	}
 }
